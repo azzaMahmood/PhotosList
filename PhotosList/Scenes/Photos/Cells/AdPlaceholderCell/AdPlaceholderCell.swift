3faa571cccc58b3ea8b1ce167633d5f2f5d7d31b
AzzA <azzaamahmoud3@gmail.com>
2022-01-08 18:39:30 +0200

    Create UI for photo and Ad placeholder cells

---
+++ PhotosList/Scenes/Photos/Cells/AdPlaceholderCell/AdPlaceholderCell.swift
@@ -0,0 +1,23 @@
+//
+//  AdPlaceholderCell.swift
+//  PhotosList
+//
+//  Created by Azza on 08/01/2022.
+//
+
+import UIKit
+
+class AdPlaceholderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- PhotosList/Scenes/Photos/PhotosViewController.swift
+++ PhotosList/Scenes/Photos/PhotosViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  PhotosViewController.swift
 //  PhotosList
 //
 //  Created by Azza on 07/01/2022.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PhotosViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
