0587fc47d6a21ed16fd8dafb78688230054a071b
AzzA <azzaamahmoud3@gmail.com>
2022-01-09 19:14:13 +0200

    Show data in PhotoDetailsViewController
    - Show the image
    - make the image zoomable 
    - make the background color of the image dominant color

--- PhotosList/Scenes/PhotoDetails/PhotoDetailsViewController.swift
+++ PhotosList/Scenes/PhotoDetails/PhotoDetailsViewController.swift
@@ -6,29 +6,74 @@
 //
 
 import UIKit
+import Kingfisher
 
-class PhotoDetailsViewController: UIViewController {
+class PhotoDetailsViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var photoImageView: UIImageView!
     
-    var viewModel = PhotoDetailsViewModel(photoDetailsViewInfo: .init(authorName: "", photoImageURL: nil))
-    
+     var viewModel: PhotoDetailsViewModel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupImageView()
+        setupScrollView()
+        setupViewBackgroundColor()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func initialize(with viewModel: PhotoDetailsViewModel) {
+        self.viewModel = viewModel
     }
-    */
+    private func setupImageView() {
+        photoImageView.layer.cornerRadius = 8
+        photoImageView.clipsToBounds = true
+        photoImageView.contentMode = .scaleAspectFit
+        photoImageView.kf.setImage(with: viewModel.getPhotoDownloadURL())
+    }
+    
+    private func setupScrollView() {
+        scrollView.maximumZoomScale = 4
+        scrollView.minimumZoomScale = 1
+        scrollView.delegate = self
+    }
 
+    private func setupViewBackgroundColor() {
+        guard let image = photoImageView.image else { return }
+        if let dominantColor = image.averageColor {
+            self.view.backgroundColor = dominantColor
+        }
+    }
+}
+
+// MARK: - UIScrollViewDelegate
+
+extension PhotoDetailsViewController {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return photoImageView
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        if scrollView.zoomScale > 1 {
+            if let image = photoImageView.image {
+                let ratioW = photoImageView.frame.width / image.size.width
+                let ratioH = photoImageView.frame.height / image.size.height
+                
+                let ratio = ratioW < ratioH ? ratioW : ratioH
+                let newWidth = image.size.width * ratio
+                let newHeight = image.size.height * ratio
+                
+                let conditionLeft = newWidth * scrollView.zoomScale > photoImageView.frame.width
+                let left = 0.5 * (conditionLeft ? newWidth - photoImageView.frame.width : (scrollView.frame.width - scrollView.contentSize.width))
+                
+                let conditionTop = newHeight * scrollView.zoomScale > photoImageView.frame.height
+                let top = 0.5 * (conditionTop ? newHeight - photoImageView.frame.height : (scrollView.frame.height - scrollView.contentSize.height))
+                
+                scrollView.contentInset = UIEdgeInsets(top: top, left: left, bottom: top, right: left)
+            }
+        }
+        else {
+            scrollView.contentInset = .zero
+        }
+    }
 }
